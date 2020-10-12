db4778cacf92ef60b77eef22d4ff839accf47e10
Matt Ewell <matt.ewell@gmail.com>
2020-10-12 08:27:19 -0600

    Add a quote factory

--- spec/models/quote_spec.rb
+++ spec/models/quote_spec.rb
@@ -6,4 +6,19 @@ describe Quote do
   it { is_expected.to belong_to(:episode) }
   it { is_expected.to validate_presence_of(:body) }
   it { is_expected.to validate_uniqueness_of(:body) }
+
+  it "has a factory" do
+    expect{
+      @quotes = create_list(:quote, 2)
+    }.to change{ Quote.count }.by(2)
+
+    quote = @quotes.first
+
+    expect(quote.body).to be_a String
+    expect(quote.body).to_not be_empty
+
+    expect(quote.character).to be_a(Character)
+    expect(quote.episode).to be_a(Episode)
+    expect(quote.season).to be_a(Season)
+  end
 end
---
+++ spec/support/factories/quote_factory.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  sequence(:quote_body) do |i|
+    "Defining this up here to give us room to make a sentence of some sort. "\
+    "A unique one, of course. #{i}"
+  end
+
+  factory(:quote) do
+    sequence(:body){ generate(:quote_body) }
+    character
+    season
+    episode
+  end
+end
